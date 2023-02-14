45a0d623891541ea8cdd8a8c222f67e43f35193f
lapomme-auguste <lapommeauguste@gmail.com>
2023-02-14 15:58:14 -0500

    Shooting

--- Assets/Scripts/ShootProjectile.cs
+++ Assets/Scripts/ShootProjectile.cs
@@ -7,7 +7,7 @@ public class ShootProjectile : MonoBehaviour
 {
     public GameObject projectilePrefab;
     
-    public float projectileSpeed = 100;
+    public float projectileSpeed = 20;
 
     //public AudioClip spellSFX;
 
