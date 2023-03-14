bf888b47f419cf17748d97497d3643942277d805
lapomme-auguste <lapommeauguste@gmail.com>
2023-03-14 15:45:00 -0400

    Dog

---
+++ Assets/Scripts/BossHit.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHit : MonoBehaviour
+{
+    public GameObject destroyedParticleEffect;
+
+    public int enemyHealth = 2;
+    
+    public GameObject milkPrefab;
+    
+    private GameObject _powerupParent;
+
+    void Start()
+    {
+        _powerupParent = GameObject.FindGameObjectWithTag("PowerupParent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            Destroy(collision.gameObject);
+            EnemyHurt(1);
+        } 
+    }
+
+    public void EnemyHurt(int damage)
+    {
+        enemyHealth -= damage;
+            
+        if (enemyHealth <= 0)
+        {
+            DestroyEnemy();
+        }
+    }
+
+    void DestroyEnemy()
+    {
+        Instantiate(destroyedParticleEffect, transform.position, transform.rotation);
+        EnemyManager.Instance.enemyCount -= 1;
+        EnemyManager.Instance.enemiesKilled += 1;
+        gameObject.SetActive(false);
+
+        if (Random.Range(0f, 10f) <= 5f)
+        {
+            GameObject projectile = Instantiate(milkPrefab,
+                transform.position + transform.forward, transform.rotation);
+        
+            projectile.transform.SetParent(
+                _powerupParent.transform);
+        }
+
+        FindObjectOfType<LevelManager>().SetScoreText();
+        FindObjectOfType<LevelManager>().LevelBeat();
+        Destroy(gameObject, 0.5f);
+        
+        
+    }
+}
