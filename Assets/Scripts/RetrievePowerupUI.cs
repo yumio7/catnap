963360b9143530448e96186639469127cd52373a
zakispie <lembarra.z@northeastern.edu>
2023-04-07 16:45:16 -0400

    changes to powerup UI

--- Assets/Scripts/RetrievePowerupUI.cs
+++ Assets/Scripts/RetrievePowerupUI.cs
@@ -6,27 +6,31 @@ using UnityEngine.UI;
 public class RetrievePowerupUI : MonoBehaviour
 {
     [SerializeField] private Powerup.powerupType type;
+    [SerializeField] private Sprite noPowerupSprite;
     private GameObject[] powerups;
 
     void FixedUpdate()
     {
         powerups = GameObject.FindGameObjectsWithTag("Powerup");
+        bool powerupFound = false;
         
         foreach (var powerup in powerups)
         {
             var powerupScript = powerup.GetComponent<Powerup>();
             if (powerupScript.GetSlot() == type)
             {
+                powerupFound = true;
                 var sprite = powerupScript.GetSprite();
                 if (sprite != null)
                 {
                     gameObject.GetComponent<Image>().sprite = sprite;
                 }
-                else
-                {
-                    // TODO what happens if there is no sprite?
-                }
             }
+        }
+
+        if (!powerupFound)
+        {
+            gameObject.GetComponent<Image>().sprite = noPowerupSprite;
         }
     }
 }
